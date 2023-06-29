b23035c74d62cc10cb39855bc62fe8cf25286ba6
NgoMauTruongQB <truongngo2707@gmail.com>
2023-06-29 16:38:26 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import SiteNavBar from './components/SiteNavbar.vue'
+import SiteFooter from './components/SiteFooter.vue'
+</script>
+
+<template>
+  <div class="flex flex-col h-screen">
+    <SiteNavBar/>
+    <main class="flex-grow mt-20">
+        <RouterView />
+    </main>
+    <SiteFooter/>
+  </div>
+</template>
---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,12 @@
+<template> 
+<footer class="bg-white rounded-lg dark:bg-gray-900">
+    <div class="w-full max-w-screen-xl md:py-8">
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            ©2023 
+            <a href="https://github.com/NgoMauTruongQB" class="hover:underline">
+                Author: Mau Truong.
+            </a>
+        </span>
+    </div>
+</footer>
+</template> 
